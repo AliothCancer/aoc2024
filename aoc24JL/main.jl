701270daab3a65ddf23c2e715a6a905815ec876c
AliothCancer <ortjuly7@gmail.com>
2024-12-01 18:13:02 +0100

    destructuring pair in map

--- aoc24JL/main.jl
+++ aoc24JL/main.jl
@@ -6,21 +6,26 @@ input = read("input/input_day1.txt", String)
 split_in_lines(input::String) = split(input, "\n", keepempty=false)
 
 function parse_line(line::SubString)::AbstractArray{Int}
-   @pipe line |> split |> parse.(Int, _)
+    @pipe line |> split |> parse.(Int, _)
 end
 
 to_matrix(parsed_lines::AbstractArray) = reduce(hcat, parsed_lines)
 
 parsed_input = input |>
-    split_in_lines .|>
-    parse_line |>
-    to_matrix
+               split_in_lines .|>
+               parse_line |>
+               to_matrix
 
-(col_a,col_b) = parsed_input[1,:], parsed_input[2,:]
+(col_a, col_b) = parsed_input[1, :], parsed_input[2, :]
 
 sort!(col_a)
 sort!(col_b)
 
-
-total_distance = @pipe zip(col_a,col_b) |> map(x->abs(x[1]-x[2]),_) |> sum
+total_distance = @pipe zip(col_a, col_b) |>
+                       map(
+                           pair -> begin
+                               a, b = pair
+                               abs(a - b)
+                           end, _) |>
+                       sum
 println("total distance: $total_distance")
